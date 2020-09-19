3a57841fe3dce12d3ca324ca353ab1da8407c76b
Rachel <rachel.rolon@gmail.com>
2020-09-19 13:36:24 -0500

    step eight search terms, not working completely.  displayList does not populate on open

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
-
+  displayList: Satellite[];
+  satellitesUrl: string;
   constructor() {
       this.sourceList = [];
       let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
- 
+      this.displayList = []
       window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
             //console.log(data);//data is an object containing a property named satellites. satellites has a property value of an array of objects
@@ -30,12 +31,25 @@ export class AppComponent {
             //    this.sourceList.push(satellite);
             // };
             // fetchedSatellites.forEach(makeObjectInSourcelist);
-
             
+            this.displayList = this.sourceList.slice(0);
          }.bind(this));
       }.bind(this));
- 
+   };
+
+   search(searchTerm: string): void {
+      let matchingSatellites: Satellite[] = [];
+      searchTerm = searchTerm.toLowerCase();
+      for(let i=0; i < this.sourceList.length; i++) {
+         let name = this.sourceList[i].name.toLowerCase();
+         if (name.indexOf(searchTerm) >= 0) {
+            matchingSatellites.push(this.sourceList[i]);
+         }
+      }
+      // assign this.displayList to be the array of matching satellites
+      // this will cause Angular to re-make the table, but now only containing matches
+      this.displayList = matchingSatellites;
    }
 
- 
-}
+
+};
